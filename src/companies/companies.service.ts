968b72bd88ddd03461caf32054f560a1cc4e477e
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-09-03 22:53:35 +0700

    Passing req.user

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,6 +4,7 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schema/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/user/users.interface';
 
 @Injectable()
 export class CompaniesService {
@@ -13,8 +14,14 @@ export class CompaniesService {
   ) {}
 
   // Them moi cong ty
-  create(createCompanyDto: CreateCompanyDto) {
-    return this.companyModel.create({ ...createCompanyDto });
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
   }
 
   findAll() {
