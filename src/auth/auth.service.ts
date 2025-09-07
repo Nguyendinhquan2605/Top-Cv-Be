75c4f23ec9bc8ccca4882a4b765c5c8c4c132c35
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-09-07 22:24:31 +0700

    Fix accessToken and create refresh Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,15 @@ import { UsersService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/user/users.interface';
 import { RegisterUserDto } from 'src/user/dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
   //username, pass  la 2 tham so thu vien passport nem' ve`
@@ -33,12 +36,17 @@ export class AuthService {
       email,
       role,
     };
+    const refresh_token = this.createRefreshToken(payload);
+
     return {
       access_token: this.jwtService.sign(payload),
-      _id,
-      name,
-      email,
-      role,
+      refresh_token,
+      user: {
+        _id,
+        name,
+        email,
+        role,
+      },
     };
   }
 
@@ -51,4 +59,14 @@ export class AuthService {
       createdAt: newUser?.createdAt,
     };
   }
+
+  // Refresh Token
+  createRefreshToken = (payload: any) => {
+    const refresh_token = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+      expiresIn:
+        ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) / 1000,
+    });
+    return refresh_token;
+  };
 }
